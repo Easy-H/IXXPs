adfb667c66783341b1430ab9d5eaeb77040481b8
EasyH <skysea001010@gmail.com>
2022-11-03 15:14:39 +0900

    시간 변화에 따른 유닛의 변화
    시간 변화에 따른 유닛의 변화에 관련된 코드를 추가
    ex) 관람차는 낮에만 작동한다.

--- Project_Cube/Assets/Scripts/TimeSet.cs
+++ Project_Cube/Assets/Scripts/TimeSet.cs
@@ -13,8 +13,14 @@ public class TimeSet : MonoBehaviour {
     [SerializeField] float _maxLight;
     [SerializeField] float _minLight;
 
+    float _alpha;
+
+    public static TimeSet Instance { get; private set; }
+
+    public float _gameTime;
+
     private void Start() {
-        
+        Instance = this;
     }
 
     private void Update() {
@@ -23,20 +29,24 @@ public class TimeSet : MonoBehaviour {
 
         float realTime = (float)(System.DateTime.Now.Hour * 15 + System.DateTime.Now.Minute * 0.25);
 
-        float timeAngle = Mathf.Atan2(v.x, v.z) * Mathf.Rad2Deg + realTime + 180;
-
-        Debug.Log(timeAngle);
-
+        float timeAngle = Mathf.Atan2(v.x, v.z) * Mathf.Rad2Deg + realTime;
+        
         if (timeAngle > 180) timeAngle -= 360;
         else if (timeAngle < -180) timeAngle += 360;
 
         Color color = _background.color;
 
-        float alpha = (Mathf.Abs(timeAngle) - 60f) / 60;
+        _alpha = (Mathf.Abs(timeAngle) - 60f) / 60;
 
-        _background.color = new Color(color.r, color.g, color.b, alpha);
+        _background.color = new Color(color.r, color.g, color.b, _alpha);
 
-        _light.intensity = Mathf.Clamp(1 - alpha, _minLight, _maxLight);
+        _light.intensity = Mathf.Clamp(_alpha, _minLight, _maxLight);
+
+    }
+
+    public bool IsDay() {
+        if (_alpha > 0) return true;
+        return false;
 
     }
 
--- Project_Cube/Assets/Scripts/Unit.cs
+++ Project_Cube/Assets/Scripts/Unit.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
 
+    [SerializeField] MonoBehaviour _onDay;
+    [SerializeField] MonoBehaviour _onNight;
+
     [SerializeField] Zone zone = null;
     [SerializeField] float height = 1;
+
+    private void Update()
+    {
+        if (_onDay) {
+            if (TimeSet.Instance.IsDay()) _onDay.enabled = true;
+            else _onDay.enabled = false;
+        }
+        if (_onNight)
+        {
+            if (TimeSet.Instance.IsDay()) _onNight.enabled = false;
+            else _onNight.enabled = true;
+        }
+    }
 
     public void Selected(Transform parent)
     {
